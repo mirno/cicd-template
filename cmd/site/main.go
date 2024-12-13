3695de17b180cc62f0168ec80cfdc8ffa889d7ef
Mirno Hoogendonk <mirno_hoogendonk@hotmail.com>
2024-12-13 22:19:55 +0100

    style(lint): golangci-lint + fixes | commitlint + config

--- cmd/site/main.go
+++ cmd/site/main.go
@@ -1,24 +1,27 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net/http"
 	"time"
 )
 
+const timeoutDefault = 10
+
 func main() {
 	server := &http.Server{
 		Addr:         "0.0.0.0:8080",
 		Handler:      http.FileServer(http.Dir("./static")),
-		ReadTimeout:  10 * time.Second, // Maximum duration for reading the request
-		WriteTimeout: 10 * time.Second, // Maximum duration for writing the response
-		IdleTimeout:  15 * time.Second, // Maximum duration for idle connections
+		ReadTimeout:  timeoutDefault * time.Second, // Maximum duration for reading the request
+		WriteTimeout: timeoutDefault * time.Second, // Maximum duration for writing the response
+		IdleTimeout:  timeoutDefault * time.Second, // Maximum duration for idle connections
 	}
 
 	log.Println("Server started at :8080")
 
 	// Start the server and handle errors
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+	if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 		log.Fatalf("Failed to start server: %v", err)
 	}
 }
